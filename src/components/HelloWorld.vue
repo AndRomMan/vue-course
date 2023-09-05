d805f23a83df8507d6e21ab83fc2f2c500669328
AndRomMan <demromdeveloper@gmail.com>
2023-09-05 16:49:56 +0300

    WIP: add components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,79 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { reactive } from 'vue';
 
 defineProps({
   msg: String,
+  falseMsg: String
 })
 
-const count = ref(0)
+const awsome = ref(true);
+let awsomeText = awsome.value;
+console.log(`awsome = ${awsome.value}`);
+const count = ref(0);
+const message = ref('Hi there!');
+const counter = reactive({ counterPoint: 0 });
+
+console.log(`counter.counterPoint = ${counter.counterPoint}`);
+console.log(`message.value = ${message.value}`);
+message.value = 'changed';
+console.log(`message.value = ${message.value}`);
+
+const titleClass = ref('title');
+
+function increment() {
+  // update component state
+  count.value++;
+  console.log(`count is changed ${count.value}`);
+}
+
+function toggle() {
+  console.log('--------------------');
+  console.log(`awsome before = ${awsome.value}`);
+  awsome.value = !awsome.value;
+  console.log(`awsome after = ${awsome.value}`);
+  awsomeText = awsome.value;
+  console.log(`awsomeText = ${awsomeText}`);
+  console.log('--------------------');
+}
+
+const text = ref('');
+
+function onInput(evt) {
+  text.value = evt.target.value;
+  console.log(`text is changed ${text.value}`);
+
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 v-if="awsome">{{ msg }}</h1>
+  <h1 v-else>{{ falseMsg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <h2 :class="titleClass">Make me red</h2>
+    <p>{{ message.split('').reverse().join('') }}</p>
+    <button @click="toggle">Change the Awsome is: {{ awsomeText }} </button>
+    <br>
+    <br>
+    <button type="button" @click="counter.counterPoint++">count is {{ counter.counterPoint }}</button>
+    <br>
+    <br>
+    <button type="button" @click="increment">count is {{ count }}</button>
+    <br>
+    <br>
+    <input :value="text" @input="onInput" placeholder="Type here">
+    <br>
+    <input v-model="text" placeholder="Type here">
+    <br>
+    <p>{{ text }}</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.title {
+  color: red;
 }
 </style>
+
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+
+// give each todo a unique id
+let id = 0;
+let newTodo = ref('');
+
+let todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' },
+]);
+let currentId = todos.value.length;
+
+function checkArray(inputArray) {
+  currentId = inputArray.value.length;
+  console.log(`array length = ${currentId}`);
+  inputArray.value.forEach(function (item, index) {
+    console.groupCollapsed(`todos item ${index}`);
+    console.log(`item = ${item.text}`);
+    console.groupEnd('todos item');
+  });
+}
+
+checkArray(todos);
+
+function addTodo() {
+  console.log(`newTodo input text = ${newTodo.value}`);
+
+  if (newTodo.value !== '') {
+    let newTodoElement = { id: currentId, text: newTodo.value };
+    console.log(newTodoElement);
+    todos.value.push(newTodoElement);
+    checkArray(todos);
+    newTodo.value = '';
+  }
+}
+
+function removeTodo(todo) {
+  console.log(`check delete`);
+  console.log(`todo = ${todo.id}`);
+  console.log(`todo = ${todo.text}`);
+
+  let targetId = todo.id;
+
+  todos.value = todos.value.filter(item => item.id != targetId)
+}
+
+</script>
+
+<template>
+  <div class="card">
+    <h2>List Rendering</h2>
+
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo">
+      <button>Add Todo</button>
+    </form>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+        <button @click="removeTodo(todo)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
