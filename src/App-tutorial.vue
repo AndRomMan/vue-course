cf329a40ab5e264b0a53f82ce34a50873cfa9697
AndRomMan <demromdeveloper@gmail.com>
2023-09-06 07:58:06 +0300

    WIP: start vue.js v3 course

---
+++ src/App-tutorial.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/tutorial/HelloWorld.vue';
+// import ListComponent from './components/tutorial/ListComponent.vue';
+// import ComputedComponent from './components/tutorial/ComputedComponent.vue';
+// import RefComponent from './components/tutorial/RefComponent.vue';
+// import WatchComponent from './components/tutorial/WatchComponent.vue';
+// import ChildComponent from './components/tutorial/ChildComponent.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Vue.js v3 — new course" falseMsg="Vue.js is great" />
+  <!-- <ListComponent /> -->
+  <!-- <ComputedComponent /> -->
+  <!-- <RefComponent /> -->
+  <!-- <WatchComponent /> -->
+  <!-- <ChildComponent /> -->
+</template>
+
+<style scoped></style>
